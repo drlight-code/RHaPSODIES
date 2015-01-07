1cab97591fc2e968b307f0122dabc766db77034b
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-01-07 13:47:42 +0000

    Make SkinClassifier base class destructor virtual
    CHG: ~SkinClassifier() virtual
    
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/RHaPSODIES/trunk@46774 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- RHaPSODemo/src/SkinClassifiers/SkinClassifier.hpp
+++ RHaPSODemo/src/SkinClassifiers/SkinClassifier.hpp
@@ -6,6 +6,8 @@
 namespace rhapsodies {
   class SkinClassifier {
   public:
+	  virtual ~SkinClassifier() {};
+	  
 	  virtual std::string GetName()=0;
 	  virtual bool IsSkinPixel(const unsigned char* rgb)=0;
   private:
