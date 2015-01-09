9af93c5b9efc60595f9e81237aa02496b384fac7
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-01-09 17:57:58 +0000

    Add JointDOF enum to HandModel
    ADD: JointDOF enum for indexing the joints array
    
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/RHaPSODIES/trunk@46855 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- RHaPSODemo/src/HandModel.hpp
+++ RHaPSODemo/src/HandModel.hpp
@@ -3,13 +3,53 @@
 
 namespace rhapsodies {
   class HandModel {
+  private:
 	  std::vector<float> m_vecJointAngles;
 	  VistaVector3D   m_vPosition;
 	  VistaQuaternion m_qOrientation;
 
   public:
-	  enum Joints {
-		  
+	  /* JointDOF indices are named as: FINGER_JOINT[_DIRECTION]
+	   * 
+	   * FINGER is one of:
+	   * T (thumb)
+	   * I (index)
+	   * M (middle)
+	   * R (ring)
+	   * L (little)
+	   *
+	   * JOINT is one of:
+	   * CMC (carpometacarpal)
+	   * MCP (metacarpophalangeal)
+	   * IP  (interphalangeal)
+	   * PIP (proximal interphalangeal)
+	   * DIP (distal interphalangeal)
+	   *
+	   * DIRECTION optional and one of:
+	   * A (abduction)
+	   * F (flexion)
+	   */
+	  enum JointDOF {
+		  T_CMC_F,
+		  T_CMC_A,
+		  T_MCP,
+		  T_IP,
+		  I_MCP_F,
+		  I_MCP_A,
+		  I_PIP,
+		  I_DIP,
+		  M_MCP_F,
+		  M_MCP_A,
+		  M_PIP,
+		  M_DIP,
+		  R_MCP_F,
+		  R_MCP_A,
+		  R_PIP,
+		  R_DIP,
+		  L_MCP_F,
+		  L_MCP_A,
+		  L_PIP,
+		  L_DIP
 	  }
   };
 }
