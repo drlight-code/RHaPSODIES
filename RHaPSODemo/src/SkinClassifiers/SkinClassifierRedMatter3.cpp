8994169672791f10f841304c6dd54502d6c1c04c
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-01-15 08:38:29 +0000

    Guess a fix for SkinClassifierRedMatter3
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/RHaPSODIES/trunk@46988 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- RHaPSODemo/src/SkinClassifiers/SkinClassifierRedMatter3.cpp
+++ RHaPSODemo/src/SkinClassifiers/SkinClassifierRedMatter3.cpp
@@ -17,7 +17,8 @@ namespace rhapsodies {
 		// @todo G/G is obviously a bug. have to try other
 		// combinations or email author (Maxim Barsuk). *probably* it
 		// is R/G since we are interested in the red hue range
-		if( (double(G) / double(G) - double(R) / double(B) <= -0.0905) &&
+//		if( (double(G) / double(G) - double(R) / double(B) <= -0.0905) &&
+		if( (double(R) / double(G) - double(R) / double(B) <= -0.0905) &&
 			(double(sum) / double(3*R) + double(R - G) / double(sum) <= 0.9498) )
 			return true;
 
