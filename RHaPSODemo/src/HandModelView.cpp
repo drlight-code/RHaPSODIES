cf459de36adb01106e763bd9669a05b9f267fc85
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-01-21 02:32:15 +0000

    Fix: use GL_TRIANGLES
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/RHaPSODIES/trunk@47144 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- RHaPSODemo/src/HandModelView.cpp
+++ RHaPSODemo/src/HandModelView.cpp
@@ -66,7 +66,7 @@ namespace rhapsodies {
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, m_vSphereVertexData.size());
+		glDrawArrays(GL_TRIANGLES, 0, m_vSphereVertexData.size());
 		
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
