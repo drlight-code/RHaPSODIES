2f5092cd9151cde390689db41eaa518c28d5692e
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-01-29 21:43:42 +0000

    Extend HandModel by skeletal bone extends, initialize with measured values
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/RHaPSODIES/trunk@47404 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- RHaPSODemo/src/HandModel.hpp
+++ RHaPSODemo/src/HandModel.hpp
@@ -11,7 +11,8 @@ namespace rhapsodies {
   public:
 	  HandModel();
 
-	  /* JointDOF indices are named as: FINGER_JOINT[_DIRECTION]
+	  /**
+	   * JointDOF indices are named as: FINGER_JOINT[_DIRECTION]
 	   * 
 	   * FINGER is one of:
 	   * T (thumb)
@@ -54,18 +55,86 @@ namespace rhapsodies {
 		  L_DIP
 	  };
 
+	  /**
+	   * Extent indices are named as: FINGER_BONE
+	   * 
+	   * FINGER is one of:
+	   * T (thumb)
+	   * I (index)
+	   * M (middle)
+	   * R (ring)
+	   * L (little)
+	   *
+	   * BONE is one of:
+	   * MC (metacarpal)
+	   * PP (proximal phalanx)
+	   * MP (medial phalanx)
+	   * DP (distal phalanx)
+	   *
+	   * @todo consider adding wrist/palm widths, bone diameters
+	   */
+	  enum Extent {
+		  T_MC,
+		  T_PP,
+		  T_DP,
+		  I_MC,
+		  I_PP,
+		  I_MP,
+		  I_DP,
+		  M_MC,
+		  M_PP,
+		  M_MP,
+		  M_DP,
+		  R_MC,
+		  R_PP,
+		  R_MP,
+		  R_DP,
+		  L_MC,
+		  L_PP,
+		  L_MP,
+		  L_DP
+	  }
+
 	  enum HandOrientation {
 		  LEFT_HAND,
 		  RIGHT_HAND
 	  };
-	  
-	  float GetJointAngle(JointDOF dof);
-	  void SetJointAngle(JointDOF dof, float angle);
 
-	  
+	  /**
+	   * Get a skeleton joint angle in degrees.
+	   * 
+	   * @param eDOF The joint DOF to retreive.
+	   * @return Angle in degrees.
+	   */
+	  float GetJointAngle(JointDOF eDOF);
+
+	  /**
+	   * Set a skeleton joint angle in degrees.
+	   * 
+	   * @param eDOF The joint DOF to set.
+	   * @param fAngleDegrees Angle in degrees.
+	   */
+	  void SetJointAngle(JointDOF eDOF, float fAngleDegrees);
+
+	  /**
+	   * Get a skeleton bone extent in millimeters.
+	   * 
+	   * @param eExt The bone extend to retreive.
+	   * @return Length in millimeters.
+	   */
+	  float GetExtent(Extent eExt);
+
+	  /**
+	   * Set a skeleton bone extent in millimeters.
+	   * 
+	   * @param eExt The bone extend to retreive.
+	   * @param fLengthMm Length in millimeters.
+	   */
+	  void SetExtent(Extent eExt, float fLengthMm);
 
   private:
 	  std::vector<float> m_vecJointAngles;
+	  std::vector<float> m_vecExtents;
 	  VistaVector3D   m_vPosition;
 	  VistaQuaternion m_qOrientation;
   };
