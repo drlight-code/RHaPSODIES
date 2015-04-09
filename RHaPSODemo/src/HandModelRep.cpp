c0539e40483e1f515f39ae127812ff803660e7f7
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-04-09 14:03:51 +0200

    Adapt handmodel representation to better fit anatomy

--- RHaPSODemo/src/HandModelRep.cpp
+++ RHaPSODemo/src/HandModelRep.cpp
@@ -5,26 +5,26 @@ namespace rhapsodies {
 	HandModelRep::HandModelRep() :
 		m_vecExtents(19) {
 
-		m_vecExtents[T_MC] = 47;
+		m_vecExtents[T_MC] = 37;
 		m_vecExtents[T_PP] = 44;
 		m_vecExtents[T_DP] = 37;
 
-		m_vecExtents[I_MC] = 103;
+		m_vecExtents[I_MC] = 93;
 		m_vecExtents[I_PP] = 55;
 		m_vecExtents[I_MP] = 35;
 		m_vecExtents[I_DP] = 30;
 
-		m_vecExtents[M_MC] = 94;
+		m_vecExtents[M_MC] = 84;
 		m_vecExtents[M_PP] = 65;
 		m_vecExtents[M_MP] = 43;
 		m_vecExtents[M_DP] = 32;
 
-		m_vecExtents[R_MC] = 86;
+		m_vecExtents[R_MC] = 76;
 		m_vecExtents[R_PP] = 60;
 		m_vecExtents[R_MP] = 37;
 		m_vecExtents[R_DP] = 30;
 
-		m_vecExtents[L_MC] = 77;
+		m_vecExtents[L_MC] = 67;
 		m_vecExtents[L_PP] = 47;
 		m_vecExtents[L_MP] = 28;
 		m_vecExtents[L_DP] = 27;
