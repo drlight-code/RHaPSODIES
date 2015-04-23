205bbd57473ef92e51ec0f6e09ce2433a3307308
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-04-23 22:23:35 +0200

    Remove unused variable

--- RHaPSODemo/src/SkinClassifiers/SkinClassifierDhawale.cpp
+++ RHaPSODemo/src/SkinClassifiers/SkinClassifierDhawale.cpp
@@ -13,15 +13,13 @@ namespace rhapsodies {
 		int G = rgb[1];
 		int B = rgb[2];
 
-		int sum = R+G+B;
 		int max = std::max( {R, G, B} );
 		int min = std::min( {R, G, B} );
-		
+
 		if(R > 40 && G > 20 && B > 10 &&
 		   (max - min > 10) &&
 		   abs(R-G) > 10 && R > G && R > B)
 			return true;
-
 		
 		return false;
 	}
