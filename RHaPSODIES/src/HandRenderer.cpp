61dfc5e264778a7fffefc72b850076569b29d474
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-05-02 13:43:45 +0200

    Only call glBindBuffer when uploading SSBO content in HandRenderer

--- RHaPSODIES/src/HandRenderer.cpp
+++ RHaPSODIES/src/HandRenderer.cpp
@@ -415,8 +415,9 @@ namespace rhapsodies {
 			iViewPortCount;
 		int iDrawCount = iSpheresPerViewport * iViewPortCount;
 
-		glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_idSSBOSphereTransforms);
 		if(bTransformTransfer) {
+			glBindBuffer(GL_SHADER_STORAGE_BUFFER,
+						 m_idSSBOSphereTransforms);
 			glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0,
 							sizeSSBO,
 							&m_vSphereTransforms[0]);
@@ -443,8 +444,9 @@ namespace rhapsodies {
 			iViewPortCount;
 		iDrawCount = iCylindersPerViewport * iViewPortCount;
 
-		glBindBuffer(GL_SHADER_STORAGE_BUFFER, m_idSSBOCylinderTransforms);
 		if(bTransformTransfer) {
+			glBindBuffer(GL_SHADER_STORAGE_BUFFER,
+						 m_idSSBOCylinderTransforms);
 			glBufferSubData( GL_SHADER_STORAGE_BUFFER, 0,
 							 sizeSSBO,
 							 &m_vCylinderTransforms[0]);
