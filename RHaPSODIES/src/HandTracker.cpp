abd8659f4283492e80985f08eb6a9c09c1c3c098
Patric Schmitz <patric.schmitz@rwth-aachen.de>
2015-05-21 18:15:21 +0200

    Update swarm in last iteration after all. Doesn't affect score and we need it in no-reset anyways.

--- RHaPSODIES/src/HandTracker.cpp
+++ RHaPSODIES/src/HandTracker.cpp
@@ -1088,16 +1088,13 @@ namespace rhapsodies {
 			tStart = oTimer.GetMicroTime();
 			UpdateScores();
 
-			// update swarm if not in last iteration
-			if(gen < m_oConfig.iPSOGenerations - 1) {
-				fPhiCognitive = m_oConfig.fPhiCognitiveBegin +
-					float(gen)/float(m_oConfig.iPSOGenerations-1) *
-					(m_oConfig.fPhiCognitiveEnd - m_oConfig.fPhiCognitiveBegin);
-				fPhiSocial = 4.1f - fPhiCognitive;
+			fPhiCognitive = m_oConfig.fPhiCognitiveBegin +
+				float(gen)/float(m_oConfig.iPSOGenerations-1) *
+				(m_oConfig.fPhiCognitiveEnd - m_oConfig.fPhiCognitiveBegin);
+			fPhiSocial = 4.1f - fPhiCognitive;
 
-				UpdateSwarm(fPhiCognitive, fPhiSocial);
-				tSwarmUpdate += oTimer.GetMicroTime() - tStart;
-			}
+			UpdateSwarm(fPhiCognitive, fPhiSocial);
+			tSwarmUpdate += oTimer.GetMicroTime() - tStart;
 		}
 
 		UpdateBestMatch();
